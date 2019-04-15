6fe45a36ef830b12b69b856491744485d752535b
Thomas Weng <thomas.weng11@gmail.com>
2019-04-14 21:33:37 -0400

    Modified camera grasp registration

--- scripts/camera_grasp_registration.py
+++ scripts/camera_grasp_registration.py
@@ -10,33 +10,31 @@ def get_transform_from_pose(pose):
     quat = Quat(pose[6], pose[3], pose[4], pose[5]) # Convert from ROS to pyquaternion convention
     transform = quat.transformation_matrix
     transform[0:3, 3] += pose[0:3]
-    return np.linalg.inv(transform)
+    return transform
 
-def get_px_coords(pose):
+def get_pixel_coords(pose):
     K = np.array([
         [618.976990, 0.0, 324.686005],
         [0.0, 618.997620, 234.163773],
         [0.0, 0.0, 1.0]
     ])
 
-    scale = 2.54
+    scale_x = 0.965 / 640 * K[0,0]
+    scale_y = 0.965 / 480 * K[1,1]
+    # scale_x = 1
+    # scale_y = 1
 
-    # u = K[0,0]/2.54*pose[0] / pose[2] + K[0,2]
-    # v = K[1,1]/2.54*pose[1] / pose[2] + K[1,2]
-    # return (u, v)
-
-    K[0,0] = K[0,0]/scale
-    K[1,1] = K[1,1]/scale
+    K[0,0] = K[0,0]/scale_x
+    K[1,1] = K[1,1]/scale_y
     res = np.matmul(K, pose[0:3])
     res = res / res[2]
     return res
 
-def get_grasp_pose_pixels(tool0_grasp_pose_world_frame):
+def get_grasp_pose_camera_frame(tool0_overhead_pose_world_frame, tool0_grasp_pose_world_frame):
     # Adjust tool0_grasp_pose_world_frame for tool0 offset
-    tool0_grasp_pose_world_frame[2] -= .0226
+    tool0_grasp_pose_world_frame[2] -= 0.0226
 
-    tool0_overhead_pose_world_frame = [0.07, 0.818, 0.6, -0.831469612676492, 0.5555702324599514, 0.0, 0.0]
-    world_tool0_transform = get_transform_from_pose(tool0_overhead_pose_world_frame)
+    world_tool0_overhead_transform = get_transform_from_pose(tool0_overhead_pose_world_frame)
     
     camera_tool0_transform = np.array([ 
         [0.36685286, 0.93018527, -0.01320435, -0.03807055],
@@ -44,62 +42,82 @@ def get_grasp_pose_pixels(tool0_grasp_pose_world_frame):
         [0.00390172, 0.01265536, 0.99991231, 0.14671274],
         [0.,         0.,         0.,         1.]])
     
-    world_camera_transform = np.matmul(np.linalg.inv(camera_tool0_transform), world_tool0_transform)
-    # world_camera_transform = np.matmul(camera_tool0_transform, world_tool0_transform)
-    # print("tool in world", tool0_grasp_pose_world_frame)
-    # print("camera in world tf", world_camera_transform.dot(np.array([0, 0, 0, 1])))
-    tool0_grasp_position_camera_frame = world_camera_transform.dot(np.hstack((tool0_grasp_pose_world_frame[0:3], 1)))
-    # print("tool pos in camera", tool0_grasp_position_camera_frame)
-    # tool0_grasp_position_camera_frame = np.array([0., 0.12, 0.2])
-    
-    # tool0_grasp_position_tool0_frame = np.matmul(world_tool0_transform, np.hstack((tool0_grasp_pose_world_frame[0:3], 1)))
-    # tool0_grasp_position_camera_frame = np.linalg.inv(camera_tool0_transform).dot(tooldiff)
-    # tool0_grasp_position_camera_frame = camera_tool0_transform.dot(tool0_grasp_position_tool0_frame)
-    # print("tool grasp position tool frame tf", tool0_grasp_position_tool0_frame)
+    world_camera_transform = np.linalg.inv(camera_tool0_transform).dot(np.linalg.inv(world_tool0_overhead_transform))
 
-    pixels = get_px_coords(tool0_grasp_position_camera_frame)
-    return pixels
+    # TODO double check that you are getting the grasp pose not the camera position 
+    world_tool0_grasp_transform = get_transform_from_pose(tool0_grasp_pose_world_frame)
+    tool0_grasp_pose_camera_frame = world_camera_transform.dot(world_tool0_grasp_transform)
 
-def get_pose_and_rgb_im(dirname):
+    return tool0_grasp_pose_camera_frame
+
+def get_grasp_pose_theta(grasp_pose_camera_frame):
+    # Rotation in the z axis
+    rot_mat = grasp_pose_camera_frame[0:3, 0:3]
+    quat = Quat(matrix=rot_mat)
+    th_z = quat.radians
+
+    print(th_z)
+    # if th_z > np.pi/2:
+        # th_z = th_z - np.pi
+
+    print(th_z)
+    print(np.rad2deg(th_z))
+    return -th_z
+
+def get_pose(dirname, filestr):
     files = os.listdir(dirname)
-
-    IKPlans = sorted([f for f in files if "IKPlan.yaml" in f])
-    IKPlan = IKPlans[-1]
-
-    with open("%s/%s" % (dirname, IKPlan), "r") as f:
+    snapshot = [f for f in files if filestr in f][0] 
+    with open("%s/%s" % (dirname, snapshot), "r") as f:
         lines = [line.rstrip('\n') for line in f]
         for i, line in enumerate(lines):
-            if "end_pose_world_frame" in line:
-                pose_str = line + lines[i+1]
-                pose = [float(x) for x in pose_str.split('[')[1].split(']')[0].split(',')]
-                break
-    
-    # Get RGB image
-    RGBImages = sorted([f for f in files if "RGBImage" in f], reverse=True)
-    RGBImage = RGBImages[-2]
-    rgb_im = skimage.io.imread("%s/%s" % (dirname, RGBImage))
+            if "Tool Pose:" in line:
+                pose = [float(l.split("  - ")[-1]) for l in lines[i+1:i+8]]
+                # print(pose)
+                return pose
+    return None
 
-    return pose, rgb_im
+def get_im(dirname, filestr):
+    files = os.listdir(dirname)
+    im_name = [f for f in files if filestr in f][0]
+    im = skimage.io.imread("%s/%s" % (dirname, im_name))
+    return im
 
 # Visualize grasp point
-def visualize_grasp(name, im, px, py):
-    plt.imshow(im)
-    plt.scatter(px, py, color='red', marker='o')
-    plt.savefig("%s.png" % name)
+def visualize_grasp(name, pixels, theta, grasp_pos):
+    im_overhead = get_im(dirname, "homeRGBImage")
+    im_approach = get_im(dirname, "approachRGBImage")
+    im_grasp = get_im(dirname, "grasp1RGBImage")
+
+    plt.subplot(221)
+    plt.imshow(im_overhead)
+    plt.scatter(pixels[0], pixels[1], s=5, color='red', marker='.')
+    plt.plot([pixels[0]+10*np.cos(theta), pixels[0]+30*np.cos(theta)], [pixels[1]+10*np.sin(theta), pixels[1]+30*np.sin(theta)], color='red')
+    plt.plot([pixels[0]-10*np.cos(theta), pixels[0]-30*np.cos(theta)], [pixels[1]-10*np.sin(theta), pixels[1]-30*np.sin(theta)], color='red')
+    # plt.plot([pixels[0]+10, pixels[0]+20], [pixels[1]+10, pixels[1]+20], color='red')
+    # plt.plot([pixels[0]-10, pixels[0]-20], [pixels[1]-10, pixels[1]-20], color='red')
+    plt.subplot(222)
+    plt.imshow(im_approach)
+    plt.subplot(223)
+    plt.text(0.1, 0.5, "%.2f, %.2f, %.2f" % (grasp_pos[0], grasp_pos[1], grasp_pos[2]))
+    plt.subplot(224)
+    plt.imshow(im_grasp)
+
+    plt.savefig("../out/%s.png" % name)
     plt.close()
 
-dirs = [
-    "attempt-2019-03-22-16-16-08",
-    "attempt-2019-03-22-16-16-24",
-    "attempt-2019-03-22-16-16-40",
-    "attempt-2019-03-22-16-16-58",
-    "attempt-2019-03-22-16-17-15"
-]
+if __name__ == '__main__':
+    for folder in sorted(os.listdir("../data")):
+        if "attempt" not in folder:
+            continue
 
-for dirname in dirs:
-    grasp_pose, rgb_im = get_pose_and_rgb_im("../data/" + dirname)
-    pixels = get_grasp_pose_pixels(grasp_pose)
-    print(pixels)
-    visualize_grasp(dirname, rgb_im, pixels[0], pixels[1])
+        dirname = "../data/" + folder
+        overhead_pose = get_pose(dirname, "homeSensorSnapshot.yaml")
+        grasp_pose = get_pose(dirname, "grasp1SensorSnapshot.yaml")
+
+        grasp_pose_camera_frame = get_grasp_pose_camera_frame(overhead_pose, grasp_pose)
+        pixels = get_pixel_coords(grasp_pose_camera_frame[0:4, 3])
+        theta = get_grasp_pose_theta(grasp_pose_camera_frame)
+
+        visualize_grasp(folder, pixels, theta, grasp_pose[0:3])
 
 
