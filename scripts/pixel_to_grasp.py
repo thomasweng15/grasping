914f6d38674ab22d61f175b0267be9d9ab4fdffb
Thomas Weng <thomas.weng11@gmail.com>
2019-05-07 10:48:35 -0400

    transformation visualizations

--- scripts/pixel_to_grasp.py
+++ scripts/pixel_to_grasp.py
@@ -6,52 +6,67 @@ import numpy as np
 import skimage
 from pyquaternion import Quaternion as Quat
 import cv2
+from shared import *
 np.set_printoptions(suppress=True)
 
-
-
-if __name__ == '__main__':
-    # Get inputs
-    dirname="testrun/"
-    px_coords, theta, im_overhead, _ = np.load(dirname + "params.npy")
-    px_coords = np.rint(px_coords).astype(int)
-    px_x, px_y = px_coords
-
-    # Plot grasp point in overhead rgb image
-    # plt.imshow(im_overhead)
-    # plt.scatter(px_coords[0], px_coords[1], c='red')
-    # plt.show()
-
-    # Get grasp position in overhead camera frame
+def get_planned_grasp(im_depth, px_coords, theta, pose_tool0home_w):
+    """
+    Get tool0 grasp tf based on depth image and pixel coordinates of grasp.
+    """
     K = np.array([
         [618.976990, 0.0, 324.686005],
         [0.0, 618.997620, 234.163773],
         [0.0, 0.0, 1.0]
     ])
-    fx = K[0, 0]
-    fy = K[1, 1]
-    cx = K[0, 2]
-    cy = K[1, 2]
-    im_depth = get_im(dirname, "homeDepthImage")
-    z = im_depth[px_y, px_x] / 1000.0
-    x = z * np.abs(px_x - cx) / fx
-    y = z * np.abs(px_y - cy) / fy
-    pose_eegrasp_c = np.array([x, y, z, 0, 0, 0, 1])
+    pose_eegrasp_c = get_pose_eegrasp_c(im_depth, K, px_coords, theta)
     
     # Account for tool0 offset from ee
-    ee_offset = -0.226
-    pose_tool0grasp_c = np.copy(pose_eegrasp_c)
-    pose_tool0grasp_c[2] += ee_offset
+    pose_tool0grasp_c = set_eetool0_offset(pose_eegrasp_c)
 
     # Convert grasp position to world pose
-    pose_tool0home_w = get_pose(dirname, "homeSensorSnapshot.yaml")
-
     tf_tool0_camera = np.array([
         [0.36685286, 0.93018527, -0.01320435, -0.03807055],
         [-0.9302708, 0.3668722, -0.00101334, -0.0164097],
         [0.00390172, 0.01265536, 0.99991231, 0.14671274],
         [0.,         0.,         0.,         1.]])
 
+    tf_tool0home_tool0grasp = get_tool0_tf(pose_tool0home_w, pose_tool0grasp_c, tf_tool0_camera)
+    quat = Quat(matrix=tf_tool0home_tool0grasp[0:3,0:3])
+    print(quat.elements)
+
+    return tf_tool0home_tool0grasp
+
+
+def get_pose_eegrasp_c(im_depth, K, px_coords, theta):
+    """
+    Get end effector grasp pose in overhead camera frame.
+    """
+    px_y, px_x = px_coords
+    fx = K[0, 0]
+    fy = K[1, 1]
+    cx = K[0, 2]
+    cy = K[1, 2]
+    z = im_depth[px_y, px_x] / 1000.0
+    x = z * np.abs(px_x - cx) / fx
+    y = z * np.abs(px_y - cy) / fy
+    quat = Quat(axis=[0.0, 0.0, 1.0], radians=theta)
+    # print(quat.elements)
+    qelts = quat.elements
+    pose_eegrasp_c = np.array([x, y, z, qelts[1], qelts[2], qelts[3], qelts[0]])
+    return pose_eegrasp_c
+
+def set_eetool0_offset(pose_eegrasp_c, ee_offset=-0.226):
+    """
+    Take the end effector grasp pose and return the tool0 grasp pose.
+    """
+    pose_tool0grasp_c = np.copy(pose_eegrasp_c)
+    pose_tool0grasp_c[2] += ee_offset
+    return pose_tool0grasp_c
+
+def get_tool0_tf(pose_tool0home_w, pose_tool0grasp_c, tf_tool0_camera):
+    """
+    Return the tool0 transformation to send to the motion planner.
+    """
     tf_world_tool0home = get_tf_from_pose(pose_tool0home_w)
     tf_world_camerahome = tf_world_tool0home.dot(tf_tool0_camera)
 
@@ -61,19 +76,106 @@ if __name__ == '__main__':
     tf_tool0home_tool0grasp = np.linalg.inv(tf_world_tool0home).dot(tf_world_tool0grasp)
 
     if True:
-        poses = [
-            pose_tool0home_w, 
-            tf_world_camerahome[0:3, 3], 
-            tf_world_tool0grasp[0:3,3]
+        tfs = [
+            tf_world_tool0home,
+            tf_world_camerahome
         ]
         fig = plt.figure()
         ax = plt.axes(projection='3d')
-        x = [p[0] for p in poses]
-        y = [p[1] for p in poses]
-        z = [p[2] for p in poses]
+        x = [tf[0,3] for tf in tfs]
+        y = [tf[1,3] for tf in tfs]
+        z = [tf[2,3] for tf in tfs]
         ax.scatter3D(x, y, z, s=50)
+
+        vectors = get_dir_vecs(tfs)
+        for tf, v in vectors:
+            xline = [tf[0,3], tf[0,3]+0.1*v[0]]
+            yline = [tf[1,3], tf[1,3]+0.1*v[1]]
+            zline = [tf[2,3], tf[2,3]+0.1*v[2]]
+
+            ax.plot3D(xline, yline, zline)
 
         ax.set_xlim(-0.5, 0.5)
         ax.set_ylim(0, 1)
         ax.set_zlim(0, 1)
         plt.show('3d.png')
+
+    return tf_tool0home_tool0grasp
+
+def get_dir_vecs(tfs):
+    # q = Quat([0.707, 0, 0.707, 0])
+    # # q = Quat(axis=[1, 1, 1], degrees=90)
+    # unit = np.array([
+    #     [1, 0, 0, 0],
+    #     [0, 1, 0, 1],
+    #     [0, 0, 1, 0],
+    #     [0, 0, 0, 1]])
+    # print(q.transformation_matrix)
+    # print(q.transformation_matrix*unit)
+    # print(q.conjugate.transformation_matrix)
+    # print(q.conjugate.transformation_matrix*unit)
+
+    tf = tfs[0]
+
+    v_b = [
+        [1, 0, 0, 1],
+        [0, 1, 0, 0],
+        [0, 0, 1, 0],
+        [0, 0, 0, 1]
+    ]
+    v_a = np.linalg.inv(tf)*v_b
+    # print(tf)
+    # print(v_b)
+    # print(v_a)
+
+    # unit_x = Quat([0.707, 0.707, 0, 0])
+    # unit_y = Quat([0.707, 0, 0.707, 0])
+    # unit_z = Quat([0.707, 0, 0, 0.707])
+    # print(unit_x.elements)
+    # print(unit_y.elements)
+    # print(unit_z.elements)
+    unit_x = [
+        [1, 0, 0, 1],
+        [0, 1, 0, 0],
+        [0, 0, 1, 0],
+        [0, 0, 0, 1]
+    ]
+    unit_y = [
+        [1, 0, 0, 0],
+        [0, 1, 0, 1],
+        [0, 0, 1, 0],
+        [0, 0, 0, 1]
+    ]
+    unit_z = [
+        [1, 0, 0, 0],
+        [0, 1, 0, 0],
+        [0, 0, 1, 1],
+        [0, 0, 0, 1]
+    ]
+    vecs = []
+    for tf in tfs:
+        for unit in [unit_x, unit_y, unit_z]:
+            vec = np.linalg.inv(tf)*unit
+            vecs.append((tf, vec[0:3, 3]))
+
+    return vecs
+
+if __name__ == '__main__':
+    # Get inputs
+    dirname="testrun/"
+    pixels, theta, im_overhead, _ = np.load(dirname + "params.npy")
+    pixels= np.rint(pixels).astype(int)
+    print(pixels, theta)
+
+    pose_tool0home_w = get_pose(dirname, "homeSensorSnapshot.yaml")
+    im_depth = get_im(dirname, "homeDepthImage")
+    planned_grasp_tf = get_planned_grasp(im_depth, pixels, theta, pose_tool0home_w)
+
+    # Plot grasp point in overhead rgb image
+    if False:
+        plt.imshow(im_overhead)
+        plt.scatter(pixels[0], pixels[1], c='red')
+        plt.plot([pixels[0]+10*np.cos(theta), pixels[0]+30*np.cos(theta)], [pixels[1]+10*np.sin(theta), pixels[1]+30*np.sin(theta)], color='red')
+        plt.plot([pixels[0]-10*np.cos(theta), pixels[0]-30*np.cos(theta)], [pixels[1]-10*np.sin(theta), pixels[1]-30*np.sin(theta)], color='red') 
+        plt.show()
+
